6bef0374e9faa9636774fe73c9c9506b284699cd
Jonathan Mascarenhas <mascarenhasjonathan@gmail.com>
2025-04-08 22:51:35 -0600

    landing page ready

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,12 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [...compat.extends("next/core-web-vitals"),
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off", // 🚫 Turn off this outdated rule
+    },
+  },
+];
 
 export default eslintConfig;
